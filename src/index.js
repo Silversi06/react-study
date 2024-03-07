793d5a61a82b0f1d964841e6eb0327dfff19635b
Eun <w2219@e-mirim.hs.kr>
2024-03-07 10:36:18 +0900

    index 수정

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import './jsx/jsx-with-style'*/
 // import './jsx/quiz1'
 // import './component-basic/first-component'
 // import './component-basic/animal-container'
-import './component-basic/props-basic-1'
+// import './component-basic/props-basic-1'
+import './component-basic/props-basic-3'
