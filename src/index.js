22efe52925529479ad59c3f1543d31512d97ec12
Eun <w2219@e-mirim.hs.kr>
2024-03-07 09:45:52 +0900

    index 실행

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 /*import './jsx/jsx-basic-1'
 import './jsx/jsx-basic-2'
 import './jsx/jsx-with-style'*/
-import './jsx/quiz1'
+// import './jsx/quiz1'
+// import './component-basic/first-component'
+// import './component-basic/animal-container'
+import './component-basic/props-basic-1'
