6bdc0b3fa40fe65ac30a5cdfbb01b447bfce63bb
Silversi06 <w2219@e-mirim.hs.kr>
2024-03-19 15:55:48 +0900

    react 컴포넌트 상태

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-import './component-intermediate/state-study-1'
+// import './component-intermediate/state-study-1'
+// import './component-intermediate/sol_user-profile'
+import './component-intermediate/state-study-3'
