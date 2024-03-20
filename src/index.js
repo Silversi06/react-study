b6cf47adad2e33ac0d59b1a596ca211b6b23a215
Silversi06 <w2219@e-mirim.hs.kr>
2024-03-20 13:33:06 +0900

    index 실행

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 // import './component-intermediate/state-study-1'
 // import './component-intermediate/sol_user-profile'
-import './component-intermediate/state-study-3'
+// import './component-intermediate/state-study-3'
+import './test/test2.js'
