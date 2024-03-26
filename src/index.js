188e9a211932ac040e99ace019e309a9cc730285
Silversi06 <w2219@e-mirim.hs.kr>
2024-03-26 15:10:54 +0900

    컴포넌트

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import './component-intermediate/state-study-1'
 // import './component-intermediate/sol_user-profile'
 // import './component-intermediate/state-study-3'
-import './test/test2.js'
+// import './test/test2.js'
+import './component-intermediate/counter-with-hook-caveat'
